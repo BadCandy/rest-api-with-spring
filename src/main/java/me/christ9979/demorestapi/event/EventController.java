2d5f8ca13e093126cf7ed058f6de1a702c8d3d02
BadCandy <christ99791@gmail.com>
2019-01-20 13:54:12 +0900

    feat(Test): Add eventController.createEvent() to create events.

---
+++ src/main/java/me/christ9979/demorestapi/event/EventController.java
@@ -0,0 +1,25 @@
+package me.christ9979.demorestapi.event;
+
+import org.springframework.hateoas.MediaTypes;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.net.URI;
+
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+
+@Controller
+@RequestMapping(value = "/api/events", produces = MediaTypes.HAL_JSON_UTF8_VALUE)
+public class EventController {
+
+    @PostMapping
+    public ResponseEntity createEvent(@RequestBody Event event) {
+
+        URI createdUri = linkTo(getClass()).slash("{id}").toUri();
+        event.setId(10);
+        return ResponseEntity.created(createdUri).body(event);
+    }
+}
--- src/test/java/me/christ9979/demorestapi/event/EventContorllerTests.java
+++ src/test/java/me/christ9979/demorestapi/event/EventContorllerTests.java
@@ -1,5 +1,6 @@
 package me.christ9979.demorestapi.event;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,7 +10,11 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.time.LocalDateTime;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
@@ -24,13 +29,33 @@ public class EventContorllerTests {
     @Autowired
     MockMvc mockMvc;
 
+    @Autowired
+    ObjectMapper objectMapper;
+
     @Test
     public void createEvent() throws Exception {
+
+        Event event = Event.builder()
+                .name("Spring")
+                .description("REST API Development")
+                .beginEnrollmentDateTime(LocalDateTime.of(2019, 01, 17, 8, 43, 22))
+                .closeEnrollmentDateTime(LocalDateTime.of(2019, 01, 18, 8, 43, 22))
+                .beginEventDateTime(LocalDateTime.of(2019, 01, 19, 8, 43, 22))
+                .endEventDateTime(LocalDateTime.of(2019, 01, 20, 8, 43, 22))
+                .basePrice(100)
+                .maxPrice(200)
+                .limitOfEnrollment(100)
+                .location("구디역")
+                .build();
+
         mockMvc.perform(post("/api/events/")
                     .contentType(MediaType.APPLICATION_JSON_UTF8)
                     .accept(MediaTypes.HAL_JSON)
+                    .content(objectMapper.writeValueAsString(event))
                     )
-                .andExpect(status().isCreated());
+                .andDo(print())
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("id").exists());
 
     }
 }
