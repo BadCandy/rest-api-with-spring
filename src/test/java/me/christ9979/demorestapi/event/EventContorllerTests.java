3ece16fa8434c9c730c1eaaa34cfd3b9bc3d4e50
BadCandy <christ99791@gmail.com>
2019-01-20 16:41:47 +0900

    feat(Test): Add test description annotation.

--- src/test/java/me/christ9979/demorestapi/event/EventContorllerTests.java
+++ src/test/java/me/christ9979/demorestapi/event/EventContorllerTests.java
@@ -1,6 +1,7 @@
 package me.christ9979.demorestapi.event;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import me.christ9979.demorestapi.common.TestDescription;
 import org.hamcrest.Matchers;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -37,6 +38,7 @@ public class EventContorllerTests {
     ObjectMapper objectMapper;
 
     @Test
+    @TestDescription("정상적으로 이벤트를 생성하는 테스트")
     public void createEvent() throws Exception {
 
         EventDto eventDto = EventDto.builder()
@@ -68,6 +70,7 @@ public class EventContorllerTests {
     }
 
     @Test
+    @TestDescription("입력받을 수 없는 값을 사용한 경우에 에러가 발생하는 테스트")
     public void createEvent_BadRequest() throws Exception {
 
         Event event = Event.builder()
@@ -96,6 +99,7 @@ public class EventContorllerTests {
     }
 
     @Test
+    @TestDescription("입력 값이 비어있는 경우에 에러가 발생하는 테스트")
     public void createEvent_BadRequest_EmptyInput() throws Exception {
         EventDto eventDto = EventDto.builder().build();
 
@@ -107,6 +111,7 @@ public class EventContorllerTests {
     }
 
     @Test
+    @TestDescription("입력 값이 잘못된 경우에 에러가 발생하는 테스트")
     public void createEvent_BadRequest_WrongInput() throws Exception {
         EventDto eventDto = EventDto.builder()
                 .name("Spring")
