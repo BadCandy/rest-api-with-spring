406f4057b93c624f1dd233cf7687fed16e006961
BadCandy <christ99791@gmail.com>
2019-01-20 19:30:36 +0900

    feat(EventController): Return EventResource by spring hateoas.
    EventController returns resource(data+link(rel+href)).

--- src/test/java/me/christ9979/demorestapi/event/EventContorllerTests.java
+++ src/test/java/me/christ9979/demorestapi/event/EventContorllerTests.java
@@ -65,7 +65,12 @@ public class EventContorllerTests {
                 .andExpect(header().string(HttpHeaders.CONTENT_TYPE, "application/hal+json;charset=UTF-8"))
                 .andExpect(jsonPath("free").value(false))
                 .andExpect(jsonPath("offline").value(true))
-                .andExpect(jsonPath("eventStatus").value(EventStatus.DRAFT.name()));
+                .andExpect(jsonPath("eventStatus").value(EventStatus.DRAFT.name()))
+                .andExpect(jsonPath("_links.self").exists())
+                .andExpect(jsonPath("_links.query-events").exists())
+                .andExpect(jsonPath("_links.update-event").exists())
+
+        ;
 
     }
 
--- src/test/java/me/christ9979/demorestapi/event/EventTest.java
+++ src/test/java/me/christ9979/demorestapi/event/EventTest.java
@@ -1,9 +1,13 @@
 package me.christ9979.demorestapi.event;
 
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@RunWith(JUnitParamsRunner.class)
 public class EventTest {
 
     @Test
@@ -31,66 +35,56 @@ public class EventTest {
     }
 
     @Test
-    public void testFree() {
+    @Parameters(method = "paramsForTestFree")
+    public void testFree(int basePrice, int maxPrice, boolean isFree) {
         // Given
         Event event = Event.builder()
-                .basePrice(0)
-                .maxPrice(0)
+                .basePrice(basePrice)
+                .maxPrice(maxPrice)
                 .build();
 
         // When
         event.update();
 
         // Then
-        assertThat(event.isFree()).isTrue();
+        assertThat(event.isFree()).isEqualTo(isFree);
+    }
 
-        // Given
-        event = Event.builder()
-                .basePrice(100)
-                .maxPrice(0)
-                .build();
-
-        // When
-        event.update();
-
-        // Then
-        assertThat(event.isFree()).isFalse();
-
-        // Given
-        event = Event.builder()
-                .basePrice(0)
-                .maxPrice(100)
-                .build();
-
-        // When
-        event.update();
-
-        // Then
-        assertThat(event.isFree()).isFalse();
+    private Object[] paramsForTestFree() {
+        return new Object[] {
+                new Object[] {0, 0, true},
+                new Object[] {100, 0 ,false},
+                new Object[] {0, 100, false},
+                new Object[] {100, 200, false}
+        };
     }
 
     @Test
-    public void testOffline() {
+    /**
+     * 파라미터를 넣어주는 메소드의 이름이 parametersFor*() 형식이면
+     * @Parameters에 관례적으로 메소드의 이름을 명시해주지 않아도 된다.
+     */
+    @Parameters
+    public void testOffline(String location, boolean isOffline) {
 
         // Given
         Event event = Event.builder()
-                .location("우리집")
+                .location(location)
                 .build();
 
         // When
         event.update();
 
         // Then
-        assertThat(event.isOffline()).isTrue();
+        assertThat(event.isOffline()).isEqualTo(isOffline);
+    }
 
-        // Given
-        event = Event.builder()
-                .build();
-
-        // When
-        event.update();
-
-        // Then
-        assertThat(event.isOffline()).isFalse();
+    private Object[] parametersForTestOffline() {
+        return new Object[] {
+                new Object[] {"강남", true},
+                new Object[] {null ,false},
+                new Object[] {"    ", false},
+                new Object[] {"", false}
+        };
     }
 }
